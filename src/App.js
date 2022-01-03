8d864ce3def2af7faf341a305de799a0f923a670
Jesse <jesse.mcconnell@live.ca>
2022-01-03 08:32:23 -0800

    app components added

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import NavBar from "./components/navbar/NavBar";
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Header />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
