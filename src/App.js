eb6876c08c8bfe3291576d718a301e2eec34f2df
Jesse <jesse.mcconnell@live.ca>
2022-01-04 05:59:37 -0800

    started on NavBar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import NavBar from "./components/navbar/NavBar";
-import Header from "./components/header/Header";
-import About from "./components/about/About";
-import Portfolio from "./components/portfolio/Portfolio";
-import Contact from "./components/contact/Contact";
-import Footer from "./components/footer/Footer";
+import {NavBar} from "./components/navbar/NavBar";
+import {Header} from "./components/header/Header";
+import {About} from "./components/about/About";
+import {Portfolio} from "./components/portfolio/Portfolio";
+import {Contact} from "./components/contact/Contact";
+import {Footer} from "./components/footer/Footer";
 import './App.css';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Header />
-      <About />
-      <Portfolio />
-      <Contact />
-      <Footer />
     </div>
   );
 }
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const NavBar = () => {
+    return (
+        <ul>
+            <li><a>About</a></li>
+            <li><a>Portfolio</a></li>
+            <li><a>Contact</a></li>
+        </ul>
+    )
+}
