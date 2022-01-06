1cc8065244003a33453617a535b7ecc4b892db03
Jesse <jesse.mcconnell@live.ca>
2022-01-06 04:49:57 -0800

    Begun working css for Portfolio component

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import TempProjectImg from "../../media/logo_cyber_jesse.jpg";
 import PortfolioFiller from "../../media/portfolio_filler.png";
+import PortfolioStyle from "./Portfolio.module.scss";
 
 export const Portfolio = () => {
     return (
-        <div>
-            <div>
+        <div className={PortfolioStyle.portContainer}>
+            <div className={PortfolioStyle.portTitle}>
                 <h2>Portfolio</h2>
             </div>
-            <div>
-                <div>
+            <div className={PortfolioStyle.portProjectContainer}>
+                <div className={PortfolioStyle.portSoloProjectContainer}>
                     <h5>Website 1</h5>
-                    <img src={TempProjectImg} />
+                    <img src={TempProjectImg} className={PortfolioStyle.portProjectImg} />
                 </div>
-                <div>
+                <div className={PortfolioStyle.portSoloProjectContainer}>
                     <h5>Website 2</h5>
-                    <img src={TempProjectImg} />
+                    <img src={TempProjectImg} className={PortfolioStyle.portProjectImg} />
                 </div>
-                <div>
+                <div className={PortfolioStyle.portSoloProjectContainer}>
                     <h5>Website 3</h5>
-                    <img src={TempProjectImg} />
-                </div>
-                <div>
-                    <h5>Website 4</h5>
-                    <img src={TempProjectImg} />
-                </div>
-                <div>
-                    <h5>Website 5</h5>
-                    <img src={TempProjectImg} />
+                    <img src={TempProjectImg} className={PortfolioStyle.portProjectImg} />
                 </div>
             </div>
-            <div>
-                <img src={PortfolioFiller} />
+            <div className={PortfolioStyle.portProjectContainer}>
+                <div className={PortfolioStyle.portSoloProjectContainer}>
+                    <h5>Website 4</h5>
+                    <img src={TempProjectImg} className={PortfolioStyle.portProjectImg} />
+                </div>
+                <div className={PortfolioStyle.portFillerImgContainer}>
+                    <img src={PortfolioFiller} className={PortfolioStyle.portFillerImg} />
+                </div>
+                <div className={PortfolioStyle.portSoloProjectContainer}>
+                    <h5>Website 5</h5>
+                    <img src={TempProjectImg} className={PortfolioStyle.portProjectImg} />
+                </div>
             </div>
         </div>
     )
